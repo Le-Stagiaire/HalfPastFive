9aceb36ccec57c2bf4ec3fc4c24665712a8e4033
clément <clement.plasse@kozea.fr>
2019-07-04 18:02:20 +0200

    Update start and end input when region is created, allow spacebar to trigger play/plause, delete previous region when creating one, update wavesurfer inition with new syntax, played audio part stays the same color as the unplayed part

--- static/js/main.js
+++ static/js/main.js
@@ -1,39 +1,42 @@
-(function() {
-  // utils for formatting time gathered on region
-  var formatTime = function(time) {
-    return [
-      Math.floor((time % 3600) / 60), // minutes
-      ("00" + Math.floor(time % 60)).slice(-2) // seconds
-    ].join(":");
-  };
+var wavesurfer;
+var region;
 
+function init() {
   // First create a wavesurfer instance
-  var wavesurfer = WaveSurfer.create({
+  wavesurfer = WaveSurfer.create({
     container: ".wavesurfer",
-    waveColor: "white",
-    progressColor: "grey"
+    waveColor: "red",
+    progressColor: "#ff0000",
+    plugins: [WaveSurfer.regions.create({ drag: false })]
   });
 
   wavesurfer.on("ready", function() {
     wavesurfer.enableDragSelection({ drag: false });
-    var timeline = Object.create(WaveSurfer.Timeline);
-    timeline.init({
-      wavesurfer: wavesurfer,
-      container: ".wavesurfer-timeline"
-    });
   });
 
   document.querySelector(".toggle-play").addEventListener("click", function() {
     wavesurfer.playPause();
   });
+  document.body.onkeyup = function(e) {
+    if (e.keyCode == 32) {
+      wavesurfer.playPause();
+    }
+  };
 
   wavesurfer.on("region-created", function(region) {
-    document
-      .querySelector("#start")
-      .setAttribute("value", formatTime(region["start"]));
-    document
-      .querySelector("#stop")
-      .setAttribute("value", formatTime(region["end"]));
+    wavesurfer.clearRegions();
+  });
+
+  wavesurfer.on("region-update-end", function(region) {
+    const start = formatTime(region["start"]);
+    const end = formatTime(region["end"]);
+    const [minute_start, second_start] = start.split(":");
+    const [minute_end, second_end] = end.split(":");
+    document.querySelector("#minute-start").setAttribute("value", minute_start);
+    document.querySelector("#second-start").setAttribute("value", second_start);
+    document.querySelector("#minute-end").setAttribute("value", minute_end);
+    document.querySelector("#second-end").setAttribute("value", second_end);
+    wavesurfer.seekTo(region["start"] / wavesurfer.getDuration());
   });
 
   document
@@ -62,4 +65,22 @@
 
       xhttp.send(params);
     });
-})();
+}
+
+// utils for formatting time gathered on region
+function formatTime(time) {
+  return [
+    Math.floor((time % 3600) / 60), // minutes
+    ("00" + Math.floor(time % 60)).slice(-2) // seconds
+  ].join(":");
+}
+
+// utils for removing region
+function removeRegion() {
+  wavesurfer.clearRegions();
+  wavesurfer.seekTo(0);
+  document.querySelector("#minute-start").setAttribute("value", "00");
+  document.querySelector("#minute-start").setAttribute("value", "00");
+  document.querySelector("#minute-end").setAttribute("value", "00");
+  document.querySelector("#minute-end").setAttribute("value", "00");
+}
