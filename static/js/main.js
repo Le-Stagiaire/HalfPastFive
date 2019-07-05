9af24a3263b3b07086ddcb3c933750b13aa8bc04
clément <clement.plasse@kozea.fr>
2019-07-05 17:32:15 +0200

    Add wavesurfer for selection

--- static/js/main.js
+++ static/js/main.js
@@ -1,30 +1,39 @@
 var wavesurfer;
+var wavesurferSelection;
 var region;
+var focusedWavesurfer;
 
 function init() {
   // First create a wavesurfer instance
   wavesurfer = WaveSurfer.create({
-    container: ".wavesurfer",
+    container: ".wavesurfer-wrapper",
     waveColor: "red",
     progressColor: "#ff0000",
-    plugins: [WaveSurfer.regions.create({ drag: false })]
+    plugins: [
+      WaveSurfer.regions.create({ drag: false }),
+      WaveSurfer.timeline.create({
+        container: ".wavesurfer-timeline-wrapper"
+      })
+    ]
   });
 
   wavesurfer.on("ready", () => {
     wavesurfer.enableDragSelection({ drag: false });
+    focusedWavesurfer = wavesurfer;
   });
 
   document.querySelector(".toggle-play").addEventListener("click", () => {
-    wavesurfer.playPause();
+    focusedWavesurfer.playPause();
   });
   document.body.onkeyup = function(e) {
     if (e.keyCode == 32) {
-      wavesurfer.playPause();
+      focusedWavesurfer.playPause();
     }
   };
 
   wavesurfer.drawer.on("click", region => {
     wavesurfer.clearRegions();
+    focusedWavesurfer = wavesurfer;
   });
 
   wavesurfer.on("region-created", region => {
@@ -32,15 +41,52 @@ function init() {
   });
 
   wavesurfer.on("region-update-end", region => {
+    // First remove previous wavesurferSelection
+    wavesurferSelection && wavesurferSelection.destroy();
+    // Update inputs for from post
     const start = formatTime(region["start"]);
     const end = formatTime(region["end"]);
-    const [minute_start, second_start] = start.split(":");
-    const [minute_end, second_end] = end.split(":");
-    document.querySelector("#minute-start").setAttribute("value", minute_start);
-    document.querySelector("#second-start").setAttribute("value", second_start);
-    document.querySelector("#minute-end").setAttribute("value", minute_end);
-    document.querySelector("#second-end").setAttribute("value", second_end);
+    const [minuteStart, secondStart] = start.split(":");
+    const [minuteEnd, secondEnd] = end.split(":");
+    document.querySelector("#minute-start").setAttribute("value", minuteStart);
+    document.querySelector("#second-start").setAttribute("value", secondStart);
+    document.querySelector("#minute-end").setAttribute("value", minuteEnd);
+    document.querySelector("#second-end").setAttribute("value", secondEnd);
+    // move cursor to the regions's start
     wavesurfer.seekTo(region["start"] / wavesurfer.getDuration());
+
+    var originalBuffer = wavesurfer.backend.buffer;
+    var newBuffer = wavesurfer.backend.ac.createBuffer(
+      originalBuffer.numberOfChannels,
+      originalBuffer.length,
+      originalBuffer.sampleRate
+    );
+
+    var firstListIndex = region.start * originalBuffer.sampleRate;
+    var secondListIndex = region.end * originalBuffer.sampleRate;
+    var secondListMemAlloc =
+      originalBuffer.length - region.end * originalBuffer.sampleRate;
+
+    var newList = new Float32Array(parseInt(firstListIndex));
+    var secondList = new Float32Array(parseInt(secondListMemAlloc));
+    var combined = new Float32Array(originalBuffer.length);
+
+    originalBuffer.copyFromChannel(newList, 0);
+    originalBuffer.copyFromChannel(secondList, 0, secondListIndex);
+
+    combined.set(newList);
+    combined.set(secondList, firstListIndex);
+
+    newBuffer.copyToChannel(combined, 0);
+    wavesurferSelection = WaveSurfer.create({
+      container: ".wavesurfer-selection-wrapper",
+      waveColor: "red",
+      progressColor: "#ff0000"
+    });
+    wavesurferSelection.loadDecodedBuffer(newBuffer);
+    wavesurferSelection.drawer.on("click", region => {
+      focusedWavesurfer = wavesurferSelection;
+    });
   });
 
   document.querySelector(".download-form").addEventListener("submit", e => {
@@ -77,9 +123,10 @@ function formatTime(time) {
 // utils for removing region
 function removeRegion() {
   wavesurfer.clearRegions();
+  wavesurferSelection.destroy();
   wavesurfer.seekTo(0);
   document.querySelector("#minute-start").setAttribute("value", "00");
-  document.querySelector("#minute-start").setAttribute("value", "00");
+  document.querySelector("#second-start").setAttribute("value", "00");
   document.querySelector("#minute-end").setAttribute("value", "00");
-  document.querySelector("#minute-end").setAttribute("value", "00");
+  document.querySelector("#second-end").setAttribute("value", "00");
 }
